9558e1e9096974ec854d3932e3e2c9c99674cecc
jeangalvis <campusjean2023@gmail.com>
2023-11-11 00:02:50 -0500

    feat(Controllers): :sparkles: Adding Controllers

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using API.Extensions;
 using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddApplicationServices();
 builder.Services.ConfigureRateLimiting();
 builder.Services.ConfigureApiVersioning();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+{
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First()); // Use First() as a workaround
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "UniversidadA", Version = "v1" });
+    c.SwaggerDoc("v1.1", new OpenApiInfo { Title = "UniversidadA v1.1", Version = "v1.1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+}
+);
 builder.Services.ConfigureCors();
 builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 
